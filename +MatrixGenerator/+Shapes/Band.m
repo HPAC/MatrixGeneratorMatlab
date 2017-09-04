4817b2b8f2f63f1249500818f34c3f2bb5e65ac2
mcopik <mcopik@gmail.com>
2017-09-04 20:37:24 +0200

    Add merge function for band matrices

--- +MatrixGenerator/+Shapes/Band.m
+++ +MatrixGenerator/+Shapes/Band.m
@@ -1,18 +1,38 @@
-classdef Band
+classdef Band < handle
     
     properties
         LowerBandwidth
         UpperBandwidth
+        Symmetric
     end
     
     methods
-        function obj = Band(lower, upper)
+        function obj = Band(lower, upper, varargin)
             obj.LowerBandwidth = lower;
             obj.UpperBandwidth = upper;
+            obj.Symmetric = false;
+            disp(nargin)
+            if nargin > 3
+               error('Only two or three parameters are allowed in the constructor')
+            elseif nargin == 3
+                symmetric = varargin{1};
+                if ~isa(symmetric, 'logical')
+                    error('Only logical values are allowed for this parameter')
+                end
+                obj.Symmetric = symmetric;
+            else
+                obj.Symmetric = false;
+            end
         end
         
         function band = to_band(obj, size)
             band = obj;
+        end
+        
+        function merge_with(obj, other_band)
+            obj.LowerBandwidth = min(obj.LowerBandwidth, other_band.LowerBandwidth);
+            obj.UpperBandwidth = min(obj.UpperBandwidth, other_band.UpperBandwidth);
+            obj.Symmetric = obj.Symmetric | other_band.Symmetric;
         end
     end
     
