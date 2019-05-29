6cdf62f8776fdc111330a60455ecf6a567e38604
Henrik Barthels <barthels@aices.rwth-aachen.de>
2019-05-29 10:52:54 +0200

    Using cell array for labels.
    Removed writing processed data to file.

--- +MatrixGenerator/Benchmarker.m
+++ +MatrixGenerator/Benchmarker.m
@@ -2,13 +2,12 @@ classdef Benchmarker < handle
 
     properties
         Timings = cell(0, 0);
-        Results = cell(0, 5);
         cs = rand(1, 7500000);
     end
 
     methods
 
-        function res = benchmark(obj, label, iters, f)
+        function res = benchmark(obj, labels, iters, f)
             timings = zeros(iters, 1);
 
             obj.cs = obj.cs + 1e-4;
@@ -21,20 +20,11 @@ classdef Benchmarker < handle
                 timings(i) = time;
             end
 
-            % Process results (exclude first warmup run)
-            avg_ = mean(timings(2:end));
-            stddev_ = std(timings(2:end));
-            min_ = min(timings(2:end));
-            max_ = max(timings(2:end));
-            obj.Results(size(obj.Results, 1) + 1, :) = {label avg_ stddev_ min_ max_};
-            obj.Timings(size(obj.Timings, 1) + 1, :) = {label timings(:)};
+            obj.Timings(size(obj.Timings, 1) + 1, :) = [labels {timings(:)}];
         end
 
         function save(obj, filename)
-            writetable(cell2table(obj.Results, 'VariableNames', {'algorithm', 'Time', 'StdDev', 'Min', 'Max'}),...
-                filename, 'Delimiter', '\t');
-            temp = split(filename, '.');
-            timings_filename = strcat(temp{1}, '_timings.', temp{2});
+            timings_filename = strcat(filename, '_timings.txt');
             writetable(cell2table(obj.Timings), timings_filename, 'Delimiter', '\t');
         end
 
